36f3a10e74164f6f7890bbb51985543ceaf97eec
Sebastian <swebermorales@gmail.com>
2023-01-05 18:51:06 -0800

    fixed img size

--- src/routes/work/[workId]/+page.svelte
+++ src/routes/work/[workId]/+page.svelte
@@ -1,10 +1,11 @@
 <script>
 	export let data;
+	import { Img } from 'flowbite-svelte';
 
 	let { content, title, jetpack_featured_media_url } = data.projectData;
 </script>
 
-<div class="h-screen w-auto bg-cyan-900 p-10">
+<div class="bg-cyan-900 p-10">
 	<div class="flex flex-col gap-5">
 		<div class="text-cyan-300 text-3xl text-center mb-10">
 			{title.rendered}
@@ -13,7 +14,7 @@
 			<div class="text-cyan-300 text-xl tracking-wide">
 				{@html content.rendered}
 			</div>
-			<img src={jetpack_featured_media_url} alt={title.rendered} />
+			<Img src={jetpack_featured_media_url} alt={title.rendered} />
 		</div>
 	</div>
 </div>
