6c406c69d9dbcfb9b4bd79bedb306c2c964a48e7
Sebastian <swebermorales@gmail.com>
2023-01-05 17:33:20 -0800

    basic project page styles

--- src/routes/work/[workId]/+page.svelte
+++ src/routes/work/[workId]/+page.svelte
@@ -4,10 +4,16 @@
 	let { content, title, jetpack_featured_media_url } = data.projectData;
 </script>
 
-<div class="text">
-	{@html content.rendered}
+<div class="h-screen w-auto bg-cyan-900 p-10">
+	<div class="flex flex-col gap-5">
+		<div class="text-cyan-300 text-3xl text-center mb-10">
+			{title.rendered}
+		</div>
+		<div class="grid grid-cols-2 items-center gap-10">
+			<div class="text-cyan-300 text-xl tracking-wide">
+				{@html content.rendered}
+			</div>
+			<img src={jetpack_featured_media_url} alt={title.rendered} />
+		</div>
+	</div>
 </div>
-<div class="text">
-	{title.rendered}
-</div>
-<img src={jetpack_featured_media_url} alt={title.rendered} />
