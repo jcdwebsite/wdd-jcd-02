39661e997a04834d12c7eff1e49bf13f6f338ccf
Sebastian <swebermorales@gmail.com>
2023-01-06 18:44:14 -0800

    netlify form test, should work now

--- src/lib/components/contactCard.svelte
+++ src/lib/components/contactCard.svelte
@@ -2,32 +2,42 @@
 	import { Input, Textarea } from 'flowbite-svelte';
 </script>
 
-<!-- <form
-	name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field"
-	class="bg-cyan-900 py-10 px-3 sm:px-10 flex flex-col items-center gap-3 sm:w-2/3 xl:w-1/2 mx-auto shadow-2xl"
+<form
+	name="contact"
+	method="POST"
+	netlify-honeypot="bot-field"
+	data-netlify="true"
+	class=" bg-cyan-900 py-10 px-3 sm:px-10 flex flex-col items-center gap-3 sm:w-2/3 xl:w-2/6 custom-shadow"
 >
+	<input type="hidden" name="form-name" value="contact-page" />
 	<div class="placeholder pb-6">
 		<div class="text-2xl text-cyan-400 text-center">Want to work together?</div>
 		<div class="text-xl text-cyan-400 text-center">Fill out this form to get started.</div>
 	</div>
-	<input type="hidden" name="contact" value="contact" />
-	<input required name="name" type="text" placeholder="Full Name" size="md" class="bg-white hover:border-orange-500" />
-	<input
+	<Input
+		name="name"
 		required
+		type="text"
+		placeholder="Full Name"
+		size="md"
+		class="bg-white hover:border-orange-500"
+	/>
+	<Input
 		name="address"
+		required
 		type="text"
 		placeholder="Project Address"
 		size="md"
 		class="bg-white hover:border-orange-500"
 	/>
-	<input
-		required
+	<Input
 		name="email"
+		required
 		type="email"
 		placeholder="email@example.com"
 		size="md"
 		class="bg-white hover:border-orange-500"
-	/>
+	>
 		<svg
 			slot="left"
 			aria-hidden="true"
@@ -40,30 +50,20 @@
 			/></svg
 		>
 	</Input>
-	<textarea
-		type="text"
+	<Textarea
+		name="details"
 		required
 		class="bg-white hover:border-orange-500"
 		id="textarea-id"
 		placeholder="Your scope of work, lot size, if you are working with a contractor, etc..."
 		rows="5"
-		name="message"
 	/>
-	<input
+	<button
 		type="submit"
-		value="Send"
+		value="Submit"
 		method="post"
-		class="text-xl text-orange-400 underline underline-offset-8 hover:underline-offset-2 cursor-pointer"
-	/>
-</form> -->
-
-<form name="netlify-form-example" method="POST" netlify-honeypot="bot-field" data-netlify="true">
-	<input type="hidden" name="form-name" value="netlify-form-example" />
-	<label for="name">Name</label>
-	<input name="name" id="name" required placeholder="Name" type="text" />
-	<label for="email">Email</label>
-	<input name="email" id="email" required placeholder="Email" type="email" />
-	<label for="message">Message</label>
-	<input name="message" id="message" required placeholder="Message" type="text" />
-	<input type="submit" value="Submit" />
+		class="text-xl font-bold text-orange-400 underline underline-offset-8 hover:underline-offset-2 cursor-pointer"
+	>
+		Send
+	</button>
 </form>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,32 +1,40 @@
 <script>
 	import { Input, Textarea } from 'flowbite-svelte';
-	import ContactCard from './contactCard2.svelte';
-
-	import pic from '$lib/images/TrussFinish06.jpg';
 </script>
 
 <div class="bg-cyan-900 bg-svg ">
-	<!-- <div class="flex flex-col sm:items-center py-40">
+	<div class="flex flex-col sm:items-center py-40">
 		<form
+			name="contact"
+			method="POST"
+			netlify-honeypot="bot-field"
+			data-netlify="true"
 			class=" bg-cyan-900 py-10 px-3 sm:px-10 flex flex-col items-center gap-3 sm:w-2/3 xl:w-2/6 custom-shadow"
 		>
+			<input type="hidden" name="form-name" value="contact-page" />
 			<div class="placeholder pb-6">
 				<div class="text-2xl text-cyan-400 text-center">Want to work together?</div>
 				<div class="text-xl text-cyan-400 text-center">Fill out this form to get started.</div>
 			</div>
 			<Input
+				name="name"
+				required
 				type="text"
 				placeholder="Full Name"
 				size="md"
 				class="bg-white hover:border-orange-500"
 			/>
 			<Input
+				name="address"
+				required
 				type="text"
 				placeholder="Project Address"
 				size="md"
 				class="bg-white hover:border-orange-500"
 			/>
 			<Input
+				name="email"
+				required
 				type="email"
 				placeholder="email@example.com"
 				size="md"
@@ -45,21 +53,24 @@
 				>
 			</Input>
 			<Textarea
+				name="details"
+				required
 				class="bg-white hover:border-orange-500"
 				id="textarea-id"
 				placeholder="Your scope of work, lot size, if you are working with a contractor, etc..."
 				rows="5"
-				name="message"
 			/>
 			<button
+				type="submit"
+				value="Submit"
 				method="post"
 				class="text-xl font-bold text-orange-400 underline underline-offset-8 hover:underline-offset-2 cursor-pointer"
 			>
 				Send
 			</button>
 		</form>
-	</div> -->
-	<form name="netlify-form-example" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+	</div>
+	<!-- <form name="netlify-form-example" method="POST" netlify-honeypot="bot-field" data-netlify="true">
 		<input type="hidden" name="form-name" value="netlify-form-example" />
 		<label for="name">Name</label>
 		<input name="name" id="name" required placeholder="Name" type="text" />
@@ -68,7 +79,7 @@
 		<label for="message">Message</label>
 		<input name="message" id="message" required placeholder="Message" type="text" />
 		<input type="submit" value="Submit" />
-	</form>
+	</form> -->
 </div>
 
 <style>
